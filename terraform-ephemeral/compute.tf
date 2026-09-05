cd5b650c7be3ba3b9acc90b4f6a7e687354be66b
Nicolas Pellegrin <875560+npellegrin@users.noreply.github.com>
2026-09-05 22:46:14 +0200

    Fix ephemeral instance image

--- terraform-ephemeral/compute.tf
+++ terraform-ephemeral/compute.tf
@@ -15,20 +15,19 @@ resource "openstack_compute_instance_v2" "dev_instance" {
   name        = "${local.resource_prefix}-instance"
   flavor_name = var.instance_flavor
   key_pair    = var.keypair_name
+  image_id    = data.openstack_images_image_v2.base_image.id
   # "default" is the project's built-in group when create_security_group is off. Not a placeholder, that's its literal name.
   security_groups = var.create_security_group ? [openstack_networking_secgroup_v2.dev_secgroup[0].name] : ["default"]
 
-  block_device {
-    uuid                  = data.openstack_images_image_v2.base_image.id
-    source_type           = "image"
-    destination_type      = "local"
-    boot_index            = 0
-    delete_on_termination = true
-  }
-
   network {
-    name = "Ext-Net"
+    uuid = data.openstack_networking_network_v2.external.id
   }
+}
+
+data "openstack_networking_network_v2" "external" {
+  region   = var.compute_region
+  name     = "Ext-Net"
+  external = true
 }
 
 resource "openstack_compute_volume_attach_v2" "dev_volume_attach" {
